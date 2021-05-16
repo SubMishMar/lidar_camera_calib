a7e4b8437ee8a8c74890853daf74978847034493
SubMishMar <subodh514@tamu.edu>
2021-05-16 02:25:40 -0500

    code cleaning

--- random_frame_generator/cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/usl/catkin_ws/src/pbpc-cal/random_frame_generator
-# Build directory: /home/usl/catkin_ws/src/pbpc-cal/random_frame_generator/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
--- random_frame_generator/cmake-build-debug/gtest/gtest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /usr/src/gtest
-# Build directory: /home/usl/catkin_ws/src/pbpc-cal/random_frame_generator/cmake-build-debug/gtest/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
